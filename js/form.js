e2a0a5d1390e91f4647ce597c0fd92babbcd1f90
AstrButterfly <maksutka1000@gmail.com>
2024-10-09 15:01:21 +0300

    Form normal working

--- js/form.js
+++ js/form.js
@@ -1,28 +1,45 @@
 const phoneInput = document.querySelector("#phone");
 const label = document.querySelector(".telephone");
-let countryCallingCode = "";
 
 const iti = window.intlTelInput(phoneInput, {
   initialCountry: "auto",
-  //separateDialCode: true, // TODO: make better
+  separateDialCode: true,
+  i18n: {
+    searchPlaceholder: "Знайти за номером, або назвою",
+  },
   geoIpLookup: function(callback) {
     fetch('https://ipapi.co/json/')
     .then(function(response) { return response.json(); })
-    .then(function(data) {
-      callback(data.country_code);
-      console.log(data);
-      countryCallingCode = data.country_calling_code;
-    })
-    .catch(function() {
-      callback('us');
-    });
+    .then(data => callback(data.country_code))
+    .catch(() => callback('us'));
   },
   utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
 });
 
-phoneInput.addEventListener('click', function() {
-  label.classList.add("focus");
-  showCountryContainer();
+document.addEventListener('click', (e) => {
+  const countryContainer = document.querySelector(".iti__country-container");
+
+  const phoneHasData = phoneInput.value.trim() !== "";
+
+  if (!e.target.closest('.iti') && !e.target.closest('#phone') && !phoneHasData) {
+    countryContainer.style.display = "none";
+    console.log('Removed by click');
+    label.classList.remove("focus");
+  }
+}, true);
+
+
+phoneInput.addEventListener('countrychange', function() {
+  countryCallingCode = iti.getSelectedCountryData().dialCode;
+  //console.log("Country code:", countryCallingCode);
+
+  if (countryCallingCode.length === 1) {
+    phoneInput.style.paddingLeft = "79px";
+  } else if (countryCallingCode.length === 2) {
+    phoneInput.style.paddingLeft = "93px";
+  } else if (countryCallingCode.length === 3) {
+    phoneInput.style.paddingLeft = "106px";
+  }
 });
 
 phoneInput.addEventListener('focus', function() {
@@ -30,18 +47,20 @@ phoneInput.addEventListener('focus', function() {
   showCountryContainer();
 });
 
-
-phoneInput.addEventListener('blur', function() {
-  const countryContainer = document.querySelector(".iti__country-container");
-
-  if (phoneInput.value === countryCallingCode) {
-    if (countryContainer) {
-      countryContainer.style.display = "none";
-      phoneInput.value = "";
-    }
-    label.classList.remove("focus");
-  }
-});
+// phoneInput.addEventListener('blur', function() {
+//   const countryContainer = document.querySelector(".iti__country-container");
+//   const ifNotEmpty = countryCallingCode + phoneInput.value;
+//   // console.log(countryCallingCode);
+//   // console.log(phoneInput.value);
+//   // console.log(ifNotEmpty);
+//   if (ifNotEmpty === countryCallingCode) {
+//     if (countryContainer) {
+//       countryContainer.style.display = "none";
+//       phoneInput.value = "";
+//     }
+//     label.classList.remove("focus");
+//   }
+// });
 
 function showCountryContainer() {
   phoneInput.setAttribute("readonly", "true");
@@ -51,11 +70,24 @@ function showCountryContainer() {
 
     if (countryContainer) {
       countryContainer.style.display = "block";
-
-      if (countryCallingCode && phoneInput.value === "") {
-        phoneInput.value = countryCallingCode;
-      }
     }
     phoneInput.removeAttribute("readonly");
-  }, 200);
+  }, 100);
+  console.log('Showed');
 }
+
+
+
+// document.querySelector('.pre-footer__contact-form').addEventListener('submit', function(event) {
+//   event.preventDefault();
+
+//   const fullPhoneNumber = `+${iti.getSelectedCountryData().dialCode}${phoneInput.value.replace(/\s+/g, '')}`;
+//   phoneInput.value = fullPhoneNumber;
+
+//   const formData = new FormData(event.target);
+
+//   for (let [name, value] of formData) {
+//     console.log(`${name}: ${value}`);
+//   }
+// });
+
