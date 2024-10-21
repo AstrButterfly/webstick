fbd3e63d07a76e02fc7699b056d322426eead3f5
AstrButterfly <maksutka1000@gmail.com>
2024-10-21 17:59:27 +0300

    media 1000px+

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
 const phoneInputs = document.querySelectorAll('input[type="tel"]');
-const labels = document.querySelectorAll('.telephone');
+const labels = document.querySelectorAll(".telephone");
 
-if (typeof window.intlTelInput != 'undefined') {
+if (typeof window.intlTelInput != "undefined") {
   phoneInputs.forEach((phoneInput, index) => {
     const iti = window.intlTelInput(phoneInput, {
       initialCountry: "auto",
@@ -9,21 +9,24 @@ if (typeof window.intlTelInput != 'undefined') {
       i18n: {
         searchPlaceholder: "Знайти за номером, або назвою",
       },
-      geoIpLookup: function(callback) {
-        fetch('https://ipapi.co/json/')
-          .then(response => response.json())
-          .then(data => callback(data.country_code))
-          .catch(() => callback('us'));
+      geoIpLookup: function (callback) {
+        fetch("https://ipapi.co/json/")
+          .then((response) => response.json())
+          .then((data) => callback(data.country_code))
+          .catch(() => callback("us"));
       },
-      utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
+      utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
     });
 
-    phoneInput.addEventListener('countrychange', function () {
+    phoneInput.addEventListener("countrychange", function () {
       const countryCallingCode = iti.getSelectedCountryData().dialCode;
-      phoneInput.style.paddingLeft = `${79 + (countryCallingCode.length - 1) * 14}px`;
+      phoneInput.style.paddingLeft = `${
+        79 + (countryCallingCode.length - 1) * 14
+      }px`;
     });
 
-    phoneInput.addEventListener('focus', function () {
+    phoneInput.addEventListener("focus", function () {
       const label = labels[index];
       label.classList.add("focus");
       showCountryContainer(phoneInput);
@@ -31,37 +34,45 @@ if (typeof window.intlTelInput != 'undefined') {
   });
 }
 
-document.addEventListener('click', (e) => {
-  const countryContainers = document.querySelectorAll(".iti__country-container");
+document.addEventListener(
+  "click",
+  (e) => {
+    const countryContainers = document.querySelectorAll(
+      ".iti__country-container"
+    );
 
-  phoneInputs.forEach((phoneInput, index) => {
-    if (!e.target.closest('.iti') && !e.target.closest(`input[type="tel"]#${phoneInput.id}`) && !phoneInput.value.trim()) {
-      const countryContainer = countryContainers[index];
-      if (countryContainer) {
-        countryContainer.style.display = "none";
-        labels[index].classList.remove("focus");
+    phoneInputs.forEach((phoneInput, index) => {
+      if (
+        !e.target.closest(".iti") &&
+        !e.target.closest(`input[type="tel"]#${phoneInput.id}`) &&
+        !phoneInput.value.trim()
+      ) {
+        const countryContainer = countryContainers[index];
+        if (countryContainer) {
+          countryContainer.style.display = "none";
+          labels[index].classList.remove("focus");
+        }
       }
-    }
-  });
-}, true);
-
+    });
+  },
+  true
+);
 
 function showCountryContainer(phoneInput) {
   phoneInput.setAttribute("readonly", "true");
 
   setTimeout(function () {
-    const countryContainer = phoneInput.closest('.iti').querySelector(".iti__country-container");
+    const countryContainer = phoneInput
+      .closest(".iti")
+      .querySelector(".iti__country-container");
 
     if (countryContainer) {
       countryContainer.style.display = "block";
     }
     phoneInput.removeAttribute("readonly");
   }, 100);
-  console.log('Showed');
+  console.log("Showed");
 }
-
-
-
 
 // document.querySelector('.pre-footer__contact-form').addEventListener('submit', function(event) {
 //   event.preventDefault();
@@ -75,4 +86,3 @@ function showCountryContainer(phoneInput) {
 //     console.log(`${name}: ${value}`);
 //   }
 // });
-
